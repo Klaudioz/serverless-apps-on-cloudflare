490f17fafce34c54b00ada3bf5b7c3e8a2b0ad21
Ashley Peacock <ashleyscool@hotmail.co.uk>
2024-08-17 21:55:08 +0100

    Add API for live weather

--- weather-app/src/app/layout.tsx
+++ weather-app/src/app/layout.tsx
@@ -16,8 +16,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <section className="vh-100">
+          <div className="container py-5 h-100">
+
+            <div className="row d-flex justify-content-center 
+                align-items-center h-100">
+              <div className="col-md-8 col-lg-6 col-xl-4">
+                {children}
+              </div>
+            </div>
+          </div>
+        </section>
       </body>
     </html>
   )
 }
+
