27c732a53a3ffd8093e10d93f51eabae29e337a5
Ashley Peacock <ashleyscool@hotmail.co.uk>
2024-08-17 21:40:12 +0100

    Add static weather app

---
+++ weather-app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Weather App',
+  description: 'See the weather, anywhere in the world (sort of)',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
