51341021ae64ec39a4ca93d7dba00c9c3b5105cd
Ashley Peacock <ashleyscool@hotmail.co.uk>
2024-08-17 18:22:30 +0100

    Add persistence

--- photo-service/src/handlers/create_image.ts
+++ photo-service/src/handlers/create_image.ts
@@ -1,21 +1,48 @@
-import { ALL_IMAGES } from "../data/image_store";
 import { IRequest } from 'itty-router'
+import { Env } from '../env';
 
-const createImage = async(request: IRequest) => {
-  const imageRequest = await request.json()
+const createImage = async(request: IRequest, env: Env) => {
+  const json = await request.json()
+  let result;
 
-  const newImage = {
-    id: parseInt(imageRequest.id),
-    url: imageRequest.url,
-    author: imageRequest.author
+  try {
+    result = await env.DB.prepare(`
+      INSERT INTO images
+      (category_id, user_id, image_url, title, format, resolution, file_size_bytes)
+      VALUES
+      (?1, ?2, ?3, ?4, ?5, ?6, ?7)
+      RETURNING *;`
+    )
+    .bind(
+      json.category_id,
+      json.user_id,
+      json.image_url,
+      json.title,
+      json.format,
+      json.resolution,
+      json.file_size_bytes
+    )
+    .first()
+  } catch (e) {
+    let message;
+    if (e instanceof Error) message = e.message;
+
+    console.log({
+      message: message
+    });
   }
 
-  ALL_IMAGES.unshift(newImage)
+  if (!result) {
+    return new Response('An error occurred', { status: 500 })
+  }
 
-  return new Response(JSON.stringify(newImage), {
-    status: 201,
-    headers: { 'content-type': 'application/json' }
-  });
+  return new Response(
+    JSON.stringify(result), 
+    {
+      status: 201,
+      headers: { 'Content-type': 'application/json' }
+    }
+  );
 }
 
 export default createImage;
--- photo-service/src/index.ts
+++ photo-service/src/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'itty-router'
 import getImages from './handlers/get_images'
 import createImage from './handlers/create_image';
 import getSingleImage from './handlers/get_single_image';
+import { Env } from './env';
 
 const router = Router()
 
@@ -10,16 +11,12 @@ router.get('/images', getImages)
       .post('/images', createImage)
       .get('*', () => new Response('Not found', { status: 404 }));
 
-export interface Env {
-  // MY_KV_NAMESPACE: KVNamespace;
-}
-
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return router.fetch(request);
+    return router.fetch(request, env);
   },
 };
